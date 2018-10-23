8c5a6142f7fcbf8639cd831f411ba73b8463ea7c
Justin Torrance <justin.torrance91@gmail.com>
2018-10-23 07:59:32 -0600

    Begin creating character info card

--- src/Characters.js
+++ src/Characters.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './styles/App.css';
+import CharacterInfoCard from './CharacterInfoCard.js'
 
 
 class Characters extends Component {
   render() {
     return (
       <div className="characters-page">
+        <CharacterInfoCard />
         <h1>Characters</h1>
+        }
       </div>
     )
   }
