388c2b73c22c1f1e38648e016a7bffb41f47a5ed
Jake Lauer <jake.lauer27@gmail.com>
2018-10-23 17:18:25 -0600

    Refactor Compare Grid construction

--- src/Compare.js
+++ src/Compare.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import characters from './data';
+import { characters, categories } from './data';
 import CompareItem from './CompareItem'
 import './styles/App.css';
+
 
 class Compare extends Component {
   constructor() {
@@ -81,37 +82,14 @@ class Compare extends Component {
           <tbody>
           <tr>
             <th><img className='compare_smash_logo'src='./images/universe_icons/flame_smash_bros.svg'/></th>
-            <th onClick={e => this.sortTable(e, 'name')} 
-                className='up table-header-name'>Name</th>
-            <th className='up table-header-tier'>Tier</th>
-            <th onClick={e => this.sortTable(e, 'tier', 'rank')} 
-                className='up table-header-rank'>Rank</th>
-            <th onClick={e => this.sortTable(e, 'world_stats', 'wins')} 
-                className='up table-header-wins'>Win</th>
-            <th onClick={e => this.sortTable(e, 'world_stats', 'losses')} 
-                className='up table-header-losses'>Loss</th>
-            <th onClick={e => this.sortTable(e, 'weight', 'class')} 
-                className='up table-header-weight-class'>Weight Cls</th>
-            <th onClick={e => this.sortTable(e, 'weight', 'weight_value')} 
-                className='up table-header-weight'>Wt</th>
-            <th onClick={e => this.sortTable(e, 'jump_height')} 
-                className='up table-header-jump-height'>Jmp Ht</th>
-            <th onClick={e => this.sortTable(e, 'speeds', 'initial_dash')} 
-                className='up table-header-dash'>Dash</th>
-            <th onClick={e => this.sortTable(e, 'speeds', 'run_speed')} 
-                className='up table-header-run'>Rn Spd</th>
-            <th onClick={e => this.sortTable(e, 'speeds', 'air_speed')} 
-                className='up table-header-air'>Air Spd</th>
-            <th onClick={e => this.sortTable(e, 'speeds', 'fall_speed')} 
-                className='up table-header-fall'>Fl Spd</th>
-            <th onClick={e => this.sortTable(e, 'speeds', 'fast_fall_speed')} 
-                className='up table-header-fast-fall'>Fst Fl Spd</th>
-            <th onClick={e => this.sortTable(e, 'counter')} 
-                className='up table-header-counter'>Ctr</th>
-            <th onClick={e => this.sortTable(e, 'strongest_smash', 'attack')} 
-                className='up table-header-smash'>Strongest Smash</th>
-            <th onClick={e => this.sortTable(e, 'strongest_smash', 'damage')} 
-                className='up table-header-smash-damage'>Smash Dmg</th>
+            {
+             categories.map( category => {
+              return <th onClick={e => this.sortTable(e, category.key1, category.key2)} className='up'>
+                      {category.name} 
+                      <span><img className='arrow' src='./images/general/arrow.svg'/></span>
+                    </th>
+             })
+            }
           </tr>
             {
               this.state.data.map( (character, i) => {
--- src/data.js
+++ src/data.js
@@ -4314,4 +4314,80 @@ const stages = [
   }
 ]
 
-export default characters;
+const categories = [ 
+  {
+    name: 'Name',
+    key1: 'name',
+    key2: false
+  }, 
+  {
+    name: 'Tier',
+    key1: 'tier',
+    key2: 'tier'
+  }, 
+  {
+    name: 'Rank',
+    key1: 'tier',
+    key2: 'rank'
+  }, 
+  {
+    name: 'Win',
+    key1: 'world_stats',
+    key2: 'wins'
+  }, 
+  {
+    name: 'Loss',
+    key1: 'world_stats',
+    key2: 'losses'
+  }, 
+  {
+    name: 'Weight Cls',
+    key1: 'weight',
+    key2: 'class'
+  }, 
+  {
+    name: 'Wt',
+    key1: 'weight',
+    key2: 'weight_value'
+  }, 
+  {
+    name: 'Jump',
+    key1: 'jump_height',
+  }, 
+  {
+    name: 'Dash',
+    key1: 'speeds',
+    key2: 'initial_dash'
+  }, 
+  {
+    name: 'Run',
+    key1: 'speeds',
+    key2: 'run_speed'
+  }, 
+  {
+    name: 'Air',
+    key1: 'speeds',
+    key2: 'air_speed'
+  }, 
+  {
+    name: 'Fall',
+    key1: 'speeds',
+    key2: 'fall_speed'
+  }, 
+  {
+    name: 'Ctr',
+    key1: 'counter',
+    key2: false
+  }, 
+  {
+    name: 'Top Smash',
+    key1: 'strongest_smash',
+    key2: 'damage'
+  }, 
+]
+
+export {
+  characters, 
+  stages,
+  categories
+};
