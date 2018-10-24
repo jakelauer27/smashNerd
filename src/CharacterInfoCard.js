7454015b16cbe1e86335053fac2b4b580cd8beab
Jake Lauer <jake.lauer27@gmail.com>
2018-10-24 17:28:03 -0600

    Refactored Character Info Card Creation

--- src/CharacterInfoCard.js
+++ src/CharacterInfoCard.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { cardCategories } from './data';
+import { cardProsCons } from './data';
 import './styles/App.css';
 
 class CharacterInfoCard extends Component {
@@ -9,71 +11,45 @@ class CharacterInfoCard extends Component {
    }
  }
 
- // componentDidMount() {
- //   fetch('http://whateverly-datasets.herokuapp.com/api/v1/characters')
- //     .then(response => response.json())
- //     .then(characters => {
- //       this.setState({
- //       characters: characters.characters
- //       })
- //     })
- //     .catch(error => console.log(error))
- // }
-
-//add event listener to div
-//onClick, target character's object
-//character's info card renders to DOM
-
-
   render() {
     console.log('this.props:', this.props.character)
     if(this.props.character === '') {
       return ( <div></div> )
     } else return (
       <div className='character-info-card'>
-        {
-          <div className='div-wrapper'>
-            <section className='character-card-image-section'>
-                      <img className='character-card-info-image' src={this.props.character.images.large} />
-                    </section>
-                    <section className='character-card-info-stats'>
-                       <h1 className='character-card-info-name'>{this.props.character.name}</h1>
+        <div className='div-wrapper'>
+          <section className='character-card-image-section'>
+            <img className='character-card-info-image' src={this.props.character.images.large} />
+          </section>
+          <section className='character-card-info-stats'>
+            <h1 className='character-card-info-name'>{this.props.character.name}</h1> 
+            {
+              cardCategories.map( category => {
+                return <div className='character-stats-div'>
+                          <p className={`character-card-title-${category.name.toLowerCase()}`}>
+                          {category.name}</p>
+                          <p className={`character-card-info-${category.name.toLowerCase()}`}>
+                          {this.props.character[category.key1][category.key2]}</p>
+                       </div>
+              })
+            }
+            {
+              cardProsCons.map( category => {
 
-                      <div className='character-stats-div'>
-                        <p className='character-card-title-universe'>Universe</p>
-                        <p className='character-card-info-universe'>{this.props.character.universe.name}</p>
-                      </div>
-
-                      <div className='character-stats-div'>
-                        <p className='character-card-title-tier'>Tier</p>
-                        <p className='character-card-info-tier'>{this.props.character.tier.tier}</p>
-                      </div>
-
-                      <div className='character-stats-div'>
-                        <p className='character-card-title-rank'>Rank</p>
-                        <p className='character-card-info-rank'>{this.props.character.tier.rank}</p>
-                      </div>
-
-                      <div className='character-stats-div'>
-                        <p className='character-card-title-weight'>Weight Class</p>
-                        <p className='character-card-info-weight'>{this.props.character.weight.class}</p>
-                      </div>
-
-                      <div className='character-stats-div'>
-                        <p className='character-card-title-pros'>Pros</p>
-                        <p className='character-card-info-pros'>{this.props.character.pros.map(pro => pro)}</p>
-                      </div>
-
-                      <div className='character-stats-div'>
-                        <p className='character-card-title-cons'>Cons</p>
-                        <p className='character-card-info-cons'>{this.props.character.cons.map(con => con)}</p>
-                      </div>
-
-                      <p className='character-card-info-link'><a href="www.link-to-wiki.com">{this.props.character.smash_wiki}</a> </p>
-                    </section>
-                    </div>
-  
-        }
+                return <div className='character-stats-div'>
+                          <p className={`character-card-title-${category.name.toLowerCase()}`}>
+                          {category.name}</p>
+                          {
+                            this.props.character[category.key].map( item => {
+                              return <p className={`character-card-info-${category.key}`}>{item}</p>
+                            })
+                          }
+                       </div>
+              } )
+            }
+            <a href={this.props.character.smash_wiki} className='character-card-info-link'>Learn More</a>
+          </section>
+        </div>
       </div>
     )
   }
