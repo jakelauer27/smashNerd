e88b67b8dedb23d95af806a3756565482b6a7d47
Elizabeth Yeh <esnider1699@gmail.com>
2018-10-24 21:01:24 -0600

    stage cards hooked up onClick

--- src/Stages.js
+++ src/Stages.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './styles/App.css';
+import StagesCards from './StagesCards.js';
 
 class Stages extends Component {
   constructor() {
     super();
     this.state = {
-      data: []
+      stages: [],
+      card: '',
+      currentStage: '',
+      nextStage: ''
     } 
 }
 
@@ -14,10 +18,20 @@ class Stages extends Component {
       .then(response => response.json())
       .then(stages => {
         this.setState({
-          data: stages.stages
+          stages: stages.stages
         })
       })
       .catch(error => console.log(error));
+    }
+
+    selectStage(e) {
+      let stage = this.state.stages.find((stage) => {
+        return stage.name.replace(/\s/g, '') === e.target.classList[0];
+      })
+      this.setState({
+        currentStage: e.target.classList[0],
+        card: stage
+      })
     }
 
 
@@ -27,13 +41,14 @@ class Stages extends Component {
         <h1 className='stages-header'>STAGES</h1>
       <section className='stages-body'>
       {  
-        this.state.data.map((stage) => {
-          return <div className='stage-cards'>
-                   <h2>{stage.name}</h2>
-                   <img src={stage.stage_image} class='stage-image' />
+        this.state.stages.map((stage) => {
+          return <div onClick={e => this.selectStage(e)} className={`${stage.name.replace(/\s/g, '')} stage-cards`}>
+                   <h2 onClick={e => this.selectStage(e)} className={stage.name.replace(/\s/g, '')}>{stage.name}</h2>
+                   <img onClick={e => this.selectStage(e)} className={`${stage.name.replace(/\s/g, '')} stage-image`} src={stage.stage_image} />
                  </div>
         })
       } 
+      <StagesCards stage={this.state.card}/>
       </section>
       </div> 
     )
--- src/StagesCards.js
+++ src/StagesCards.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles/App.css';
 
 
+class StagesCards extends Component {
+  constructor() {
+    super();
+    this.state = {
+      stages: []
+    }
+  }
 
+  render() {
+    if(this.props.stage === '') {
+      return ( <div></div> )
+    } else return (
+      <div className='stage-info-card' >
+        <div className='stage-wrap' >
+          <section className='stage-card-image-wrap' >
+            <img className='stage-card-image' src={this.props.stage.stage_image} />
+          </section>
+        </div>
+      </div>
+      )
+  }
+
+
+}
 
 
 
