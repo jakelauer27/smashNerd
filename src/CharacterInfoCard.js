0fb97fc1ababea829b40fd4bc6c451932ebc1d9d
Jake Lauer <jake.lauer27@gmail.com>
2018-10-26 23:10:39 -0600

    Finish first pass at styling player cards

--- src/CharacterInfoCard.js
+++ src/CharacterInfoCard.js
@@ -9,7 +9,9 @@ class CharacterInfoCard extends Component {
     if(this.props.character === '') {
       return ( <div></div> )
     } else return (
+      <div className='arrow-wrapper'>
       <div className='character-info-card'>
+        <div className='triangle'></div>
           <div className='card top-row'>
           <section className='card card-name-container'>
             <h1 className='card card-name'>{this.props.character.name}</h1> 
@@ -17,7 +19,7 @@ class CharacterInfoCard extends Component {
           </section>
           <section className='card tier-container'>
             <h2 className='card card-tier-label'>Tier:</h2> 
-            <h1 className='card card-tier'>{this.props.character.tier.tier}</h1> 
+            <h1 className='card card-tier' id={this.props.character.tier.tier}>{this.props.character.tier.tier}</h1> 
           </section>
           </div>
           <div className='main-card-content'>
@@ -54,9 +56,10 @@ class CharacterInfoCard extends Component {
             <a href={this.props.character.smash_wiki} target='_blank' className='card card-link'>Learn More</a>
           </div>
         </div>
-        <button onClick={e => this.props.scrollCard(e)} className='left-button'>Left</button>
-        <button onClick={e => this.props.scrollCard(e)} className='right-button'>Right</button>
-        <button className='delete-button'>Delete</button>
+      </div>
+        <button onClick={e => this.props.scrollCard(e)} className='left-button scroll-button'><i class="fas fa-angle-left"></i></button>
+        <button onClick={e => this.props.scrollCard(e)} className='right-button scroll-button'><i class="fas fa-angle-right"></i></button>
+        <button className='delete-button scroll-button'><i class="fas fa-times"></i></button>
       </div>
     )
   }
