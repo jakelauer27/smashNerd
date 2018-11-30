50ec10698f5a6c4e1c613b828f10016dfbcc7dac
Jake Lauer <jake.lauer27@gmail.com>
2018-11-30 13:00:50 -0700

    Updates

---
+++ precache-manifest.c12bf7cad2e88b37c847615ba3863ed5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c7d68cfaf29aeb6e7fb133aca9ecbe2",
+    "url": "/static/media/smash_font-webfont.1c7d68cf.woff"
+  },
+  {
+    "revision": "7504102e511fb5113b39",
+    "url": "/static/css/main.57b10853.chunk.css"
+  },
+  {
+    "revision": "338a2fe60ae3dd5a0b25",
+    "url": "/static/js/1.338a2fe6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "05230e6355d9ea1e8f3467980d0e099f",
+    "url": "/static/media/smash_font-webfont.05230e63.woff2"
+  },
+  {
+    "revision": "ef086065072234382dc61983151856f2",
+    "url": "/static/media/mosaic.ef086065.png"
+  },
+  {
+    "revision": "7504102e511fb5113b39",
+    "url": "/static/js/main.7504102e.chunk.js"
+  },
+  {
+    "revision": "49be4aa83b59e92e46ba03d2a46b7a9c",
+    "url": "/static/media/smash_font.49be4aa8.ttf"
+  },
+  {
+    "revision": "1053335fe47d9cb8c03f1942ad78b7e7",
+    "url": "/static/media/final_destination.1053335f.jpg"
+  },
+  {
+    "revision": "8e8d23f019fb8ae94801c1bf22772dc2",
+    "url": "/static/media/night_battlefield.8e8d23f0.jpg"
+  },
+  {
+    "revision": "23aad1af866dbf38562f0499986ae50c",
+    "url": "/static/media/battlefield_cover.23aad1af.jpg"
+  },
+  {
+    "revision": "1626fc4e8a662c088fe53fc5e4fe9708",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/smashNerd/precache-manifest.ea5933e99470605b1a2ec9670c3b8f02.js"
+  "/precache-manifest.c12bf7cad2e88b37c847615ba3863ed5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/smashNerd/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
